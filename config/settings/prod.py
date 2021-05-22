cd59a283d0d19a019f0def94b6ba0edfc1bb298d
ammduncan <ammduncan@gmail.com>
2021-05-22 15:49:10 +0000

    correctly placed whitenoise middleware

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -19,8 +19,8 @@ DATABASES = {
 }
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
